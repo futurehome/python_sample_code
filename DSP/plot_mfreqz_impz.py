8407344cfe2481e677d81d8a3131ccf4ffd166c4
Alex Wang <wangb2@conexant.com>
2018-03-25 00:24:46 +0800

    add zoom windows

--- DSP/plot_mfreqz_impz.py
+++ DSP/plot_mfreqz_impz.py
@@ -36,22 +36,68 @@ def mfreqz(b, a=1, **kwargs):
             ax1.axvline(kwargs[key], color='r', ls='--', lw=0.5)
         if key == 'PassBandStop':
             ax1.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+            PassBandStop = kwargs[key]
         if key == 'PassBandRipple':
             ax1.axhline(y=-2*kwargs[key], color='r', ls='--', lw=0.5)
             #ax1.annotate('Pass Band Ripple', xy=(0, -2*PassBandRipple-4), color='r')
         if key == 'PassBandEdge':
             ax1.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+            PassBandEdge = kwargs[key]
         if key == 'StopBandEdgeBandPass':
             ax1.axvline(kwargs[key], color='r', ls='--', lw=0.5)
         if key == 'StopBandEdge':
             ax1.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+            StopBandEdge = kwargs[key]
         if key == 'StopBandAttenu':
             ax1.axhline(-kwargs[key], color='r', ls='--', lw=0.5)
+            StopBandAttenu = kwargs[key]
     #ax1.annotate('Pass Band Ripple', xy=(0, -2*PassBandRipple-4), color='r')
     ax1.set_ylabel('Magnitude (db)')
     #ax1.set_xlabel(r'Normalized Frequency (x$\pi$rad/sample)')
     ax1.set_xlabel('Frequency (Hz)')
     ax1.set_title(r'Frequency response')
+
+    axzoomup = fig.add_axes([0.15, 0.77, 0.25, 0.1])
+    axzoomup.set_xlim(PassBandStop-50000, PassBandEdge+10000)
+    axzoomup.set_ylim(-5, 1)
+    axzoomup.plot(w_hz, h_dB)
+    for key in kwargs:
+        if key == 'PassBandStart':
+            axzoomup.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'PassBandStop':
+            axzoomup.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'PassBandRipple':
+            axzoomup.axhline(y=-2*kwargs[key], color='r', ls='--', lw=0.5)
+            #ax1.annotate('Pass Band Ripple', xy=(0, -2*PassBandRipple-4), color='r')
+        if key == 'PassBandEdge':
+            axzoomup.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'StopBandEdgeBandPass':
+            axzoomup.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'StopBandEdge':
+            axzoomup.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'StopBandAttenu':
+            axzoomup.axhline(-kwargs[key], color='r', ls='--', lw=0.5)
+
+    axzoomdn = fig.add_axes([0.15, 0.64, 0.25, 0.1])
+    axzoomdn.set_xlim(StopBandEdge-10000, StopBandEdge+50000)
+    axzoomdn.set_ylim(-StopBandAttenu-100, -StopBandAttenu+50)
+    axzoomdn.plot(w_hz, h_dB)
+    for key in kwargs:
+        if key == 'PassBandStart':
+            axzoomdn.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'PassBandStop':
+            axzoomdn.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'PassBandRipple':
+            axzoomdn.axhline(y=-2*kwargs[key], color='r', ls='--', lw=0.5)
+            #ax1.annotate('Pass Band Ripple', xy=(0, -2*PassBandRipple-4), color='r')
+        if key == 'PassBandEdge':
+            axzoomdn.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'StopBandEdgeBandPass':
+            axzoomdn.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'StopBandEdge':
+            axzoomdn.axvline(kwargs[key], color='r', ls='--', lw=0.5)
+        if key == 'StopBandAttenu':
+            axzoomdn.axhline(-kwargs[key], color='r', ls='--', lw=0.5)
 
     ax2 = fig.add_subplot(212, sharex=ax1)
     h_Phase = np.unwrap(np.arctan2(np.imag(h), np.real(h)))
@@ -63,6 +109,8 @@ def mfreqz(b, a=1, **kwargs):
     ax2.set_title(r'Phase response')
     plt.subplots_adjust(hspace=0.5)
     _ = MultiCursor(fig.canvas, (ax1, ax2), color='r', lw=1)
+
+    plt.tight_layout()
     plt.show()
 
 
